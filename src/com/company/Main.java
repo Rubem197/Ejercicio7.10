1aa3d359d18641cfc00582d26e2f29fbc0ff4576
Rubem197 <ruben.lindes@iesnervion.es>
2022-02-03 19:24:30 +0100

    awa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Gato gato1 = new Gato("Andres", "Rojo", "Europeo", 4);
-    Gato gato2 = new Gato("Felipe", "Blanco", "Comun", 5);
+        Gato gato1 = new Gato("Andres", "Rojo", "Europeo", 4);
+        Gato gato2 = new Gato("Felipe", "Blanco", "Comun", 5);
 
-    gato1.compararNombre(gato2);
-    gato1.compararEdad(gato2);
+        gato1.compararNombre(gato2);
+        gato1.compararEdad(gato2);
     }
 }
